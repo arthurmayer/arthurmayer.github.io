5aa1ef324b52fdf574b141e96e49bce4297b30ce
Arthur Mayer <arthurmayer@mymail.mines.edu>
2018-04-30 13:06:09 -0600

    Fixed bugs

--- test.js
+++ test.js
@@ -14,34 +14,34 @@ var highScore = 0;
 var bricks = [];
 var poles = [];
 var difficulty = 5; // 5, 6, 7
-var difficultyDict = {'easy' : 5, 'medium':6, 'hard':7};
-var movesDict = {1:50, 5:50, 6:100, 7:200};
-var poleTops = [0,0,0];
+var difficultyDict = { 'easy': 5, 'medium': 6, 'hard': 7 };
+var movesDict = { 1: 50, 5: 50, 6: 100, 7: 200 };
+var poleTops = [0, 0, 0];
 var isMuted = false;
 var scoreName = "";
 // BUTTON FUNCTIONS
-function setName(){
+function setName() {
     scoreName = window.prompt("Enter a name for the highscore", "Hanoi").toString();
-    if (typeof(Storage) != "undefined") {
+    if (typeof (Storage) != "undefined") {
         localStorage.setItem("HanoiName", scoreName);
     } else {
         //alert no highscore kept
         alert("Sorry, your browser does not support Web Storage, Highscore will not be saved");
     }
     //update highscore if any update
-    document.getElementById("scorename").innerHTML="Scored by: " + localStorage.getItem("HanoiName").toString();
+    document.getElementById("scorename").innerHTML = "Scored by: " + localStorage.getItem("HanoiName").toString();
 }
 //reset name
-function resetName(){
+function resetName() {
     // Check browser support
-    if (typeof(Storage) != "undefined") {
+    if (typeof (Storage) != "undefined") {
         localStorage.setItem("HanoiName", "Hanoi");
     } else {
         //alert no highscore kept
         alert("Sorry, your browser does not support Web Storage, Highscore will not be saved");
     }
     //update highscore if any update
-    document.getElementById("scorename").innerHTML="Scored by: " + localStorage.getItem("HanoiName").toString();
+    document.getElementById("scorename").innerHTML = "Scored by: " + localStorage.getItem("HanoiName").toString();
 }
 // manage the mute functionality of the game
 function mute() {
@@ -59,8 +59,8 @@ function mute() {
 }
 function difficultyChange(ev) {
     elem = document.getElementById("difficultyRadio");
-    for (var i = 0; i< elem.length; i++) {
-        if(elem[i].checked) {
+    for (var i = 0; i < elem.length; i++) {
+        if (elem[i].checked) {
             output = (elem[i].value);
             difficulty = difficultyDict[elem[i].value];
         }
@@ -97,16 +97,19 @@ function drop(ev) {
             tar = document.getElementById((parseInt(tar.id, 10) + 3).toString());
         }
     }
-    
-    if (tar.id <= 3 * (difficulty - 1)){
+
+    if (tar.id <= 3 * (difficulty - 1)) {
         canDrop = document.getElementById((parseInt(tar.id, 10) + 3)).innerHTML > image;
     }
-    if (srcId > 3){
+    if (srcId > 3) {
         if (document.getElementById((parseInt(srcId, 10) - 3)).innerHTML != "") {
             canDrop = false;
         }
     }
-    if(canDrop ){
+    if (ev.target.tagName == "IMG" || ev.target.innerHTML != "") {
+        canDrop = false;
+    }
+    if (canDrop) {
         document.getElementById("get").play();
     }
     if (!canDrop && !isMuted) {
@@ -128,27 +131,27 @@ function drop(ev) {
     if (moveCount == 0) {
         setTimeout(alert("You are out of moves!"), 3000);
     }
-    if (document.getElementById('3').innerHTML!=''){
+    if (document.getElementById('3').innerHTML != '') {
         alert("you win!");
-        console.log('Player wins with '+moveCount + ' movevs left!');
+        console.log('Player wins with ' + moveCount + ' movevs left!');
         saveScore(moveCount);
     }
 }
 
-function reset(){
-	createTable();
+function reset() {
+    createTable();
 }
 
 //function call for saving score
-function saveScore(score){
+function saveScore(score) {
     // Check browser support
-    if (typeof(Storage) != "undefined") {
+    if (typeof (Storage) != "undefined") {
         //called first so set name 
-        if(localStorage.getItem("HanoiName") == null){
+        if (localStorage.getItem("HanoiName") == null) {
             localStorage.setItem("HanoiName", "Hanoi");
         }
         // Store if score is greater than highscore
-        if(score > localStorage.getItem("HighscoreHanoi")){
+        if (score > localStorage.getItem("HighscoreHanoi")) {
             localStorage.setItem("HighscoreHanoi", score);
             localStorage.setItem("HanoiName", prompt("Please Enter your Name, Winner!", 'Hanoi'));
         }
@@ -157,16 +160,16 @@ function saveScore(score){
         alert("Sorry, your browser does not support Web Storage, Highscore will not be saved");
     }
     //update highscore if any update
-    document.getElementById("highscore").innerHTML="Best Move Count: " + localStorage.getItem("HighscoreHanoi").toString();
-    document.getElementById("scorename").innerHTML="Scored by: " + localStorage.getItem("HanoiName").toString();
+    document.getElementById("highscore").innerHTML = "Best Move Count: " + localStorage.getItem("HighscoreHanoi").toString();
+    document.getElementById("scorename").innerHTML = "Scored by: " + localStorage.getItem("HanoiName").toString();
 }
 //reset highscore
-function resetHighScore(){
+function resetHighScore() {
     localStorage.setItem("Highscore", 150);
-    document.getElementById("highscore").innerHTML="Best Move Count: 0";
-    document.getElementById("scorename").innerHTML="Scored by: " 
+    document.getElementById("highscore").innerHTML = "Best Move Count: 0";
+    document.getElementById("scorename").innerHTML = "Scored by: "
     localStorage.setItem("HanoiName", 'Hanoi');
-    localStorage.setItem("HighscoreHanoi",0);
+    localStorage.setItem("HighscoreHanoi", 0);
 }
 
 
@@ -186,6 +189,7 @@ function changeId(img) {
 }
 
 
+
 function createTable() {
     var rowNum = 0;
     var index = 0;
@@ -193,23 +197,39 @@ function createTable() {
     moveCount = movesDict[difficulty];
     document.getElementById("move_count").innerHTML = ("Move Count: " + moveCount);
     table.innerHTML = "";
-	for (var j=0; j<difficulty; j++){
-		var row = table.insertRow(rowNum);
-		rowNum++;
-		for(var i=0; i<3; i++) {
-			//var ID = j*DIMENSION + i + 1;
-			var newcell = row.insertCell(i);
-			if (i==0){
-			    newcell.innerHTML = "<div id=" + (++index).toString() + " ondragstart='changeId(this)' ondrop='drop(event)' ondragover='allowDrop(event)'><img src='./images/hanoi" + (j + 1).toString() + ".png' onclick='changeId(this)' draggable='true' ondragstart='drag(event)' width='100%' id='img" + index.toString() + "'></div>";
-			}
-			else{
-			    newcell.innerHTML = "<div id=" + (++index).toString() + " ondragstart='changeId(this)' ondrop='drop(event)' ondragover='allowDrop(event)' width='100%'></div>";
-			}
-		}
-	}
+    for (var j = 0; j < difficulty; j++) {
+        var row = table.insertRow(rowNum);
+        rowNum++;
+        for (var i = 0; i < 3; i++) {
+            //var ID = j*DIMENSION + i + 1;
+            var newcell = row.insertCell(i);
+            if (i == 0) {
+                newcell.innerHTML = "<div id=" + (++index).toString() + " ondragstart='changeId(this)' ondrop='drop(event)' ondragover='allowDrop(event)'><img src='./images/hanoi" + (j + 1).toString() + ".png' onclick='changeId(this)' draggable='true' ondragstart='drag(event)' width='100%' id='img" + index.toString() + "'></div>";
+            }
+            else {
+                newcell.innerHTML = "<div id=" + (++index).toString() + " ondragstart='changeId(this)' ondrop='drop(event)' ondragover='allowDrop(event)' width='100%'></div>";
+            }
+        }
+    }
 }
 
 createTable();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
