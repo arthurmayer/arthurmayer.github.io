d3841265689a4d806ebbcce1e801a1967c7b4d02
arthurmayer <arthurmayer@mymail.mines.edu>
2020-05-10 10:47:48 -0700

    Fix news

--- HCR_WEBSITE/js/news.js
+++ HCR_WEBSITE/js/news.js
@@ -7,7 +7,7 @@ function displayNews(XML){
 
     news = xml.getElementsByTagName("news-item");
     news_list = document.getElementById("news-items");
-    num_to_display = parseInt(news_items.getAttribute("display_count"));
+    num_to_display = parseInt(news_list.getAttribute("display_count"));
     if (num_to_display.isNaN() || num_to_display < 1){
         num_to_display = news.length;
     }
