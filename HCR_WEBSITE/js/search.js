6013262a54dc44517226ad709b9a4a09d40c62b4
arthurmayer <arthurmayer@mymail.mines.edu>
2020-05-31 08:59:34 -0700

    Search fix

--- HCR_WEBSITE/js/search.js
+++ HCR_WEBSITE/js/search.js
@@ -1,57 +1,3 @@
-var searchIndex = lunr(function() {
-    this.ref("id");
-    this.field("title", { boost: 10 });
-    this.field("content");
-    for (var key in window.pages) {
-        this.add({
-            "id": key,
-            "title": pages[key].title,
-            "content": pages[key].content
-        });
-    }
-});
-
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] === variable) {
-          return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
-      }
-  }
-}
-
-var searchTerm = getQueryVariable("q");
-// creation of searchIndex from earlier example
-var results = searchIndex.search(searchTerm);
-var resultPages = results.map(function (match) {
-  return pages[match.ref];
-});
-
-// resultPages from previous example
-resultsString = "";
-resultPages.forEach(function (r) {
-    resultsString += "<li>";
-    resultsString +=   "<a class='result' href='" + r.url + "?q=" + searchTerm + "'><h3>" + r.title + "</h3></a>";
-    resultsString +=   "<div class='snippet'>" + r.content.substring(0, 200) + "</div>";
-    resultsString += "</li>"
-});
-if (resultsString === ""){
-    resultsString = "<p>NO RESULTS MATCHED THE SEARCH QUERY \""+searchTerm+"\"</p>"
-}
-document.getElementById("search-results").innerHTML = resultsString;
-
-
-
-
-
-
-
-/* TODO: DELETE ABOVE CODE AND UNCOMMENT BELOW CODE FOR LIVE PAGE
-
-
-
 var getHTML = function ( url, callback ) {
     // Feature detection
     if ( !window.XMLHttpRequest ) return;
@@ -80,7 +26,7 @@ for (const site of sites){
             "url": page.URL,
             "path": site
             };
-        
+
         var searchIndex = lunr(function() {
             this.ref("id");
             this.field("title", { boost: 10 });
@@ -121,9 +67,5 @@ for (const site of sites){
             resultsString += "</li>"
         });
         document.getElementById("search-results").innerHTML = resultsString;
-
     });
 }
-
-
-*/
